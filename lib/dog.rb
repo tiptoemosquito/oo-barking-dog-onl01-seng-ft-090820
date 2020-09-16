20d2c63cdb62e9d88d587b789036fe47bd521556
Christina Howard <clhoward33@icloud.com>
2020-09-16 03:30:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 # Your code goes here!
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   def name
-    this_dogs_name
+    @this_dogs_name
   end
   def bark
-    puts "Woof!"
+    @puts "Woof!"
   end
 
 end
